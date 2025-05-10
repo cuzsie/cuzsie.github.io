e4c3d2a3b8070d92ffc4af1a3503d8f509e57ec6
Cuzsie <68414886+cuzsie@users.noreply.github.com>
2025-05-10 13:45:31 -0700

    Optimizations 2

---
+++ troller/script.js
@@ -0,0 +1,12 @@
+function wega()
+{
+    var text = document.getElementById("text");
+    var wegap = document.getElementById("wegap");
+    var element = document.getElementById("wega");
+
+    document.title = "WEGA";
+    
+    text.hidden = true;
+    wegap.hidden = false;
+    element.play();
+}
