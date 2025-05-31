099b7c087cc1e089ce2303315649fac0bdfa6a93
Cuzsie <68414886+cuzsie@users.noreply.github.com>
2025-05-31 10:37:23 -0700

    Fix 0 on deltarune countdown

--- deltarunetomorrow/countdown.js
+++ deltarunetomorrow/countdown.js
@@ -14,7 +14,7 @@ function updateCountdown() {
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("timer-text").textContent = String(days + 1).padStart(2, '0');
+    document.getElementById("timer-text").textContent = String(days);
     document.getElementById("days").textContent = String(days).padStart(2, '0');
     document.getElementById("hours").textContent = String(hours).padStart(2, '0');
     document.getElementById("minutes").textContent = String(minutes).padStart(2, '0');
